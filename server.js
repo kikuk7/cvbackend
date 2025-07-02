ce07bbb09ea524027cf6d8a28f4259d1c3faa0de
Mas Muhammad Rizqi Adiguna <rizqiadiguna4@gmail.com>
2025-07-02 12:31:17 +0700

    Fix nw

--- server.js
+++ server.js
@@ -36,7 +36,8 @@ const upload = multer({
 const allowedOrigins = [
   'http://localhost:3000',
   'https://cvalams-rizqis-projects-607b9812.vercel.app',
-  'https://cvalams.vercel.app'
+  'https://cvalams.vercel.app',
+  'https://cvsumberalamraya.vercel.app/'
 ];
 
 app.use(cors({
